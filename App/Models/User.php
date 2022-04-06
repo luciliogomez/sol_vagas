0baa38b3c55aa55d46aed0abc87fcb065af11d36
luciliogomez <luciliodetales@gmail.com>
2022-04-07 00:27:53 +0100

    Updating Models

--- App/Models/User.php
+++ App/Models/User.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Models;
 
+use App\Utils\Conexao;
+
 class User{
     private $id;
 
@@ -58,4 +60,83 @@ class User{
         return $this->nivel;
     }
 
+
+
+    public function read()
+    {
+        $query = "SELECT * FROM candidato";
+
+        $stmt = Conexao::getInstance()->prepare($query);
+        $stmt->execute();
+        if($stmt->rowCount()>0){
+
+            return $stmt->fetchAll(\PDO::FETCH_CLASS,Candidato::class);
+        
+        }else{
+
+            return [];
+        
+        }
+    }
+    public function load($id)
+    {
+        $query = "SELECT id,nome,email,senha,nivel 
+        FROM users WHERE id =:id";
+
+        $stmt = Conexao::getInstance()->prepare($query);
+        $stmt->bindParam("id",$id);
+        $stmt->execute();
+        if($stmt->rowCount()>=1){
+
+            return $stmt->fetchObject(Candidato::class);
+        
+        }else{
+
+            return [];
+        
+        }
+    }
+
+
+    public function create()
+    {
+        $query = "INSERT INTO users (nome,email,senha,nivel) 
+        VALUES (:nome,:email,:senha,nivel)";
+
+        $stmt = Conexao::getInstance()->prepare($query);
+        $stmt->bindParam(":nome",$this->getNome());
+        $stmt->bindParam(":email",$this->getEmail());
+        $stmt->bindParam(":senha",$this->getSenha());
+        $stmt->bindParam(":nivel",$this->getNivel());
+        $stmt->execute();
+        if($stmt->rowCount()>=1){
+
+            return Conexao::getInstance()->lastInsertId();
+        
+        }else{
+            return null;
+        }
+    }
+
+    public function update()
+    {
+        $query = "UPDATE users SET nome = :nome, email = :email, 
+        senha = :senha, nivel = :nivel, 
+        WHERE id = :id";
+
+        $stmt = Conexao::getInstance()->prepare($query);
+        $stmt->bindParam(":nome",$this->getNome());
+        $stmt->bindParam(":email",$this->getEmail());
+        $stmt->bindParam(":senha",$this->getSenha());
+        $stmt->bindParam(":nivel",$this->getNivel());
+        $stmt->bindParam(":id",$this->getId());
+        $stmt->execute();
+        if($stmt->rowCount()>=1){
+
+            return true;
+        
+        }else{
+            return false;
+        }
+    }
 }
