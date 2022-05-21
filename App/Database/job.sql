4e212806921ff9d154225c6498f439a48213fd99
luciliogomez <luciliodetales@gmail.com>
2022-05-22 00:34:17 +0100

    Fix: remove old uploaded files

--- App/Database/job.sql
+++ App/Database/job.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 16-Maio-2022 às 15:25
+-- Tempo de geração: 22-Maio-2022 às 01:33
 -- Versão do servidor: 10.4.19-MariaDB
 -- versão do PHP: 7.4.19
 
@@ -49,10 +49,10 @@ CREATE TABLE `candidato` (
 --
 
 INSERT INTO `candidato` (`id`, `nome`, `email`, `senha`, `titulo`, `foto`, `cidade`, `telefone`, `resumo`, `habilidades`, `area`, `cv`, `ingles`, `estado`) VALUES
-(1, 'Lucílio Gomes', 'luciliodetales@gmail.com', '$2y$10$IVSSx/NxTVmcz/75Fjx0XedoSAdlLb5nbAHuJ4zKWz.bVGUxz6t2W', 'Técnico de Informática', 'sv62824d388a41a151016', 'Luanda', '943812726', 'resumo', NULL, 'ti', NULL, 'basico', 0),
+(1, 'Lucílio Gomes', 'luciliodetales@gmail.com', '$2y$10$IVSSx/NxTVmcz/75Fjx0XedoSAdlLb5nbAHuJ4zKWz.bVGUxz6t2W', 'Técnico de Informática', 'sv6289638a54da9001122', 'Luanda', '943812726', 'resumo', NULL, 'ti', NULL, 'basico', 0),
 (2, 'Pedro Viriato', 'pedro@gmail.com', '$2y$10$IVSSx/NxTVmcz/75Fjx0XedoSAdlLb5nbAHuJ4zKWz.bVGUxz6t2W', 'Enfermeiro', 'sv627e314ea4689122206', 'Huambo, Angola', '934445566', 'Amante da Vida. Sou enfermeiro de profissão, comprometido com o melhoramento da vida dos meus pacientes.', NULL, 'Saúde', NULL, 'basico', 1),
 (3, 'Joana Dos Santos', 'joana@gmail.com', '$2y$10$c0ULTtb/YwcLG.OWySwBO.i0SI2IeenKfIIp90YI.FHnaTqpx/dz.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-(4, 'Elisa Da Silva Salumbo Hebo', 'elisa@gmail.com', '$2y$10$J99YwSj8/JFrwUDky9GU3ua4km6r6jy8K3MURTKPRhA8cxfaX/j9a', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(4, 'Elisa Da Silva Salumbo Hebo', 'elisa@gmail.com', '$2y$10$J99YwSj8/JFrwUDky9GU3ua4km6r6jy8K3MURTKPRhA8cxfaX/j9a', 'Advogada', '', 'Luanda', '943556453', '', NULL, 'ti', NULL, 'basico', 0),
 (5, 'Maria Joao', 'maria@gmail.com', '$2y$10$XInK8RVDaeLSpbIbY0b4.u4OalZMCsvmIr0i.nXnejNo39kt1TV6y', 'Contabilista', 'sv628246d45eba7144300', 'Luanda', '923240284', 'Contabilista a mais de 4 anos. Proactiva, autodidata.', NULL, 'economia', NULL, 'basico', 1);
 
 -- --------------------------------------------------------
@@ -73,10 +73,13 @@ CREATE TABLE `candidatura` (
 --
 
 INSERT INTO `candidatura` (`id`, `id_candidato`, `id_vaga`, `estado`) VALUES
-(1, 1, 1, 'pendente'),
+(1, 1, 1, 'entrevista'),
 (2, 1, 2, 'pendente'),
-(3, 1, 3, 'pendente'),
-(4, 2, 2, 'pendente');
+(3, 1, 3, 'aprovado'),
+(4, 2, 2, 'pendente'),
+(5, 3, 3, 'entrevista'),
+(6, 4, 3, 'aprovado'),
+(7, 4, 7, 'pendente');
 
 -- --------------------------------------------------------
 
@@ -112,10 +115,10 @@ CREATE TABLE `empresa` (
   `nome` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `senha` varchar(255) NOT NULL,
-  `ano` int(11) NOT NULL,
-  `descricao` text NOT NULL,
-  `telefone` varchar(50) NOT NULL,
-  `cidade` varchar(200) NOT NULL,
+  `ano` int(11) DEFAULT NULL,
+  `descricao` text DEFAULT NULL,
+  `telefone` varchar(50) DEFAULT NULL,
+  `cidade` varchar(200) DEFAULT NULL,
   `logotipo` varchar(200) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -124,8 +127,9 @@ CREATE TABLE `empresa` (
 --
 
 INSERT INTO `empresa` (`id`, `nome`, `email`, `senha`, `ano`, `descricao`, `telefone`, `cidade`, `logotipo`) VALUES
-(1, 'BITLINE SOFTWARES', 'geral.bitline@gmail.com', '12345', 2020, 'Empresa de Sofware, focada no desenvolvimento de Softwares de Gestão, Web sites, etc.', '922334455', 'Luanda, Angola', NULL),
-(2, 'Clinica Pureza', 'pureza.clinica@gmail.com', '12345', 2018, 'Clinica Pureza, serviços de Saúde.', '922123456', 'Moxico', NULL);
+(1, 'BITLINE SOFTWARES', 'geral.bitline@gmail.com', '$2y$10$.0sfBAm1Oho8z2U6V6LTUuVcBVG9fizwvM4a30xD7LHiZ086oRiri', 2020, 'Empresa de Sofware, focada no desenvolvimento de Softwares de Gestão, Web sites, etc.', '922334455', 'Luanda, Angola', 'sv6283e119de369195329'),
+(2, 'Clinica Pureza', 'pureza.clinica@gmail.com', '$2y$10$.0sfBAm1Oho8z2U6V6LTUuVcBVG9fizwvM4a30xD7LHiZ086oRiri', 2019, 'Clinica Pureza, serviços de Saúde. Condições estabelecidas pelo Ministerio da Saude', '922123456', 'Luanda', 'sv6282c60236394234538'),
+(3, 'Slime Code', 'slime@gmail.com', '$2y$10$OCXuKyYsvBk6k98MSoplmu/yWSJd3T.nqoonGIjq7WedF5Rk5/Ng6', 2005, 'Empresa de Informática. Actuando no sector Nacional desde 2005 nas especialidades de Desenvolvimento de Software, hospedagens, cloud,etc.', '927553093', 'Luanda', 'sv6283b33b17de0163747');
 
 -- --------------------------------------------------------
 
@@ -228,8 +232,12 @@ CREATE TABLE `vaga` (
 
 INSERT INTO `vaga` (`id`, `area`, `titulo`, `formato`, `modalidade`, `cidade`, `salario_min`, `salario_max`, `descricao`, `habilidades`, `ano_de_experiencia`, `educacao`, `data_limite`, `estado`, `id_empresa`) VALUES
 (1, 'ti', 'Programador', 'fulltime', 'presencial', 'Luanda', 300000, NULL, 'Desenvolvedor de Software, BACKEND.\r\nDesenvolver sites, softwares de variados generos.\r\nDar manutenção aos softwares existentes na compania.', 'Desenvolvimento Web\r\n- Dinamico\r\n- Html, Css, Javascript\r\n- PHP, JAVA\r\n- Angular , Sringboot\r\n- Git\r\n- Metodologias Ageis\r\n- Apis\r\n- Clean Code', 2, 'medio', '2022-04-14', 1, 1),
-(2, 'Saúde', 'Enfermeiro', 'fulltime', 'presencial', 'Moxico', 200000, NULL, 'Tratar doentes, cuidar de doentes', 'Dinamico, Proeficiente em primeiros socorros', 1, 'licenciado', '2022-05-19', 1, 2),
-(3, 'ti', 'Programador C#', 'fulltime', 'presencial', 'Luanda', NULL, NULL, 'Desenvolver Aplicações C#', '', NULL, 'licenciatura', '2022-05-26', 1, 1);
+(2, 'Saúde', 'Enfermeiro', 'meioperiodo', 'presencial', 'Moxico', 200000, 0, 'Tratar doentes, cuidar de doentes', 'Dinamico, Proeficiente em primeiros socorros', 1, 'licenciatura', '2022-05-19', 0, 2),
+(3, 'ti', 'Programador C#', 'fulltime', 'presencial', 'Luanda', NULL, NULL, 'Desenvolver Aplicações C#', '', 1, 'licenciatura', '2022-05-26', 1, 1),
+(4, 'ti', 'UX/UI Designer', 'estagio', 'presencial', 'Luanda', 0, 0, 'd', 'd', 1, 'medio', '2022-05-21', 1, 1),
+(5, 'Saúde', 'Medico Legista', 'fulltime', 'presencial', 'Cabinda', 0, 0, 'Medico Legista', 'Saiba fazer autópsia Completa', 1, 'licenciatura', '2022-05-28', 1, 2),
+(6, 'Saúde', 'Medico', 'fulltime', 'presencial', 'Luanda', 0, 0, 'w', 'w', 0, 'licenciatura', '2022-06-09', 1, 2),
+(7, 'Saúde', 'Tecnina De Laboratorio', 'fulltime', 'presencial', 'Luanda', 0, 0, 'w', 'w', 1, 'medio', '2022-06-11', 1, 2);
 
 --
 -- Índices para tabelas despejadas
@@ -304,7 +312,7 @@ ALTER TABLE `candidato`
 -- AUTO_INCREMENT de tabela `candidatura`
 --
 ALTER TABLE `candidatura`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de tabela `curso`
@@ -316,7 +324,7 @@ ALTER TABLE `curso`
 -- AUTO_INCREMENT de tabela `empresa`
 --
 ALTER TABLE `empresa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `experiencia`
@@ -340,7 +348,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de tabela `vaga`
 --
 ALTER TABLE `vaga`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Restrições para despejos de tabelas
